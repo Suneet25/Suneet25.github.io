fd82b50bde0a46502067fddad1a38ece5c77ca2f
Suneet Panigrahi <suneetpanigrahi2505@gmail.com>
2023-05-13 00:01:31 +0530

    added changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ export const Home = () => {
     <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
       <div className="flex flex-col justify-center h-full w-full">
         <h2 className="text-5xl sm:text-5xl font-bold  text-white" >
-          Hi, I'm  <span style={{ color: "orange" }}>Suneet Panigrahi,</span>
+          Hi, I'm  <span style={{ color: "orange" }}>Suneet Panigrahi</span>
         </h2>
         <p className="py-4 max-w-md tracking-wide text-lg ">
    
